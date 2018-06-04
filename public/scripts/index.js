af7f1aaced420ec487c255b0b74fb78304375fa9
Mohammed Mulazada <thisismoniac@gmail.com>
2018-06-04 14:27:50 +0200

    added requirements

---
+++ public/scripts/index.js
@@ -0,0 +1,1 @@
+console.error("\n/Users/mohammedmulazada/Documents/web-minor/node-sass-es8-boilerplate/views/index.js:1\nimport Main from './components/main/main.js';\n^\nParseError: 'import' and 'export' may appear only with 'sourceType: module'");
